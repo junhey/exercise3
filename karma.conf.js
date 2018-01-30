e356d086645e794769d709c6f9035b75b9a468b4
junhey <hejun@modernavenue.com>
2018-01-30 11:41:42 +0800

    update

--- karma.conf.js
+++ karma.conf.js
@@ -57,14 +57,8 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome','ChromeCanary'],
+    browsers: ['Firefox'],
 	
-	customLaunchers: {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
